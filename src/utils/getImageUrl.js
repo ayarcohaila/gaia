d6d8fa0f94df03e82ed7ddd1ad6af29a80a3672d
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-05-25 14:21:46 -0300

    Adding 'ipfs://' replace (#123)

--- src/utils/getImageUrl.js
+++ src/utils/getImageUrl.js
@@ -2,7 +2,8 @@ import { ipfsGateway } from '~/config/config';
 
 export const getImageURL = (image, local) => {
   if (local) return image;
-  const imageURL = image.replace(/\s/g, '');
+  const removePrefix = image.replace('ipfs://', '');
+  const imageURL = removePrefix.replace(/\s/g, '');
   if (imageURL.match('^(https?://).*$')) return imageURL;
   return new URL(imageURL, ipfsGateway).toString();
 };
