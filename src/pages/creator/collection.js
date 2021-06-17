fa737aef101ea79fe97c861b6e8a5dde5391b47e
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-06-17 13:02:16 -0300

    Adding website field and validation (#148)
    Co-authored-by: Daniel Laurindo <daniel.laurindo@hp.com>

--- src/pages/creator/collection.js
+++ src/pages/creator/collection.js
@@ -14,6 +14,7 @@ import {
   UploadWrapper
 } from '~/components/profile/styled';
 import useAuth from '~/hooks/useAuth';
+import { regexURL } from '~/utils/validations';
 import Seo from '~/components/seo/seo';
 import { CREATE_SET } from '~/store/server/mutations';
 import { URLs } from '~/routes/urls';
@@ -72,6 +73,20 @@ const FormComponent = ({ onSubmit, loading }) => {
             }
           ]}>
           <StyledInput placeholder="Collection Name" />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ span: 24 }}
+          name="websiteUrl"
+          shouldUpdate={false}
+          label="Website Url"
+          labelCol={{ span: 24 }}
+          rules={[
+            {
+              pattern: regexURL,
+              message: 'This field must be a valid url.'
+            }
+          ]}>
+          <StyledInput placeholder="Website Url" />
         </Form.Item>
         <Row>
           <Col span={19}>
