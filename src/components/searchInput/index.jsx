d19edfc1f0346697f1d44fc0953ef564ce8e9dbe
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-10-08 13:01:02 -0300

    refactor: Refactor search input

--- src/components/searchInput/index.jsx
+++ src/components/searchInput/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { InputAdornment } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import * as Styled from './styled.js';
+import * as Styled from './styles.js';
 import PropTypes from 'prop-types';
 
-const SearchInput = ({ endAdornment = null, placeholder = 'Search', ...props }) => {
+const SearchInput = ({ endAdornment, placeholder, ...props }) => {
   return (
     <Styled.Search
       disableUnderline
@@ -24,6 +24,11 @@ const SearchInput = ({ endAdornment = null, placeholder = 'Search', ...props }) 
 SearchInput.propTypes = {
   placeholder: PropTypes.string,
   endAdornment: PropTypes.node
+};
+
+SearchInput.defaultProps = {
+  placeholder: 'Search',
+  endAdornment: null
 };
 
 export default React.memo(SearchInput);
--- src/components/searchInput/styles.js
+++ src/components/searchInput/styles.js
@@ -1,28 +1,28 @@
 import { Input, styled } from '@mui/material';
 
-export const Search = styled(Input)(({ theme }) => ({
+export const Search = styled(Input)(({ theme: { typography, palette } }) => ({
   width: '305px',
   height: '40px',
   border: 'none',
   marginLeft: 'auto',
   padding: '12px 16px 12px 20px',
   borderRadius: '20px',
-  backgroundColor: theme.palette.grey[300],
+  backgroundColor: palette.grey[300],
   cursor: 'pointer',
 
   '& > .MuiInput-input': {
     fontWeight: 'bold',
-    color: theme.palette.grey[700],
+    color: palette.grey[700],
     cursor: 'pointer',
-    ...theme.typography.subtitle1
+    ...typography.subtitle1
   },
 
   '& > .MuiInput-input::placeholder': {
-    color: theme.palette.grey[600]
+    color: palette.grey[600]
   },
 
   '&:hover > .MuiInput-input::placeholder': {
-    color: theme.palette.grey[700],
+    color: palette.grey[700],
     opacity: 1
   }
 }));
