ce37eb56f10f106952e9f2df0e3a3313181047be
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-10-11 09:21:56 -0300

    Fix: use single import

--- src/components/collectionBannerAccount/styles.js
+++ src/components/collectionBannerAccount/styles.js
@@ -1,5 +1,4 @@
-import { styled } from '@mui/material';
-import { Typography } from '@mui/material';
+import { styled, Typography } from '@mui/material';
 
 const BannerAccountStyled = styled('div', { shouldForwardProp: prop => prop !== 'bgColor' })(
   ({ bgColor }) => ({
