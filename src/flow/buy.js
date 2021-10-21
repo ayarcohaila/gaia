11b93d18d50166e52833b4914232c61768ee96db
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-10-21 18:15:22 -0300

    feat: integrate buy and sell
    - added new buy and sell transactions

--- src/flow/buy.js
+++ src/flow/buy.js
@@ -1,48 +1,55 @@
 import { fcl, t } from '../config/config';
 
 const BUY_NFT_TX = fcl.cdc`
-import FungibleToken from 0xFungibleToken
-import NonFungibleToken from 0xNFTInterface
-import Gaia from 0xNFTContract
-import FUSD from 0xFUSDContract
-import GaiaMarket from 0xNFTMarket
+import FungibleToken from "../contracts/FungibleToken.cdc"
+import NonFungibleToken from "../contracts/NonFungibleToken.cdc"
+import FlowToken from "../contracts/FlowToken.cdc"
+import Gaia from "../contracts/Gaia.cdc"
+import NFTStorefront from "../contracts/NFTStorefront.cdc"
 
-transaction(saleAssetID: UInt64, address: Address) {
+transaction(listingResourceID: UInt64, storefrontAddress: Address) {
     let paymentVault: @FungibleToken.Vault
-    let AssetsCollection: &Gaia.Collection{NonFungibleToken.Receiver}
-    let marketCollection: &GaiaMarket.Collection{GaiaMarket.CollectionPublic}
+    let GaiaCollection: &Gaia.Collection{NonFungibleToken.Receiver}
+    let storefront: &NFTStorefront.Storefront{NFTStorefront.StorefrontPublic}
+    let listing: &NFTStorefront.Listing{NFTStorefront.ListingPublic}
 
-    prepare(signer: AuthAccount) {
-        let FUSDVaultStoragePath = /storage/fusdVault
+    prepare(acct: AuthAccount) {
+        self.storefront = getAccount(storefrontAddress)
+            .getCapability<&NFTStorefront.Storefront{NFTStorefront.StorefrontPublic}>(
+                NFTStorefront.StorefrontPublicPath
+            )!
+            .borrow()
+            ?? panic("Could not borrow Storefront from provided address")
 
-        self.marketCollection = getAccount(address)
-                    .getCapability<&GaiaMarket.Collection{GaiaMarket.CollectionPublic}>(GaiaMarket.CollectionPublicPath).borrow()
-                    ?? panic("Could not borrow capability from public collection")
+        self.listing = self.storefront.borrowListing(listingResourceID: listingResourceID)
+                    ?? panic("No Offer with that ID in Storefront")
+        let price = self.listing.getDetails().salePrice
 
-        let saleItem = self.marketCollection.borrowSaleItem(itemID: saleAssetID)
-                    ?? panic("No item with that ID")
-        let price = saleItem.price
-
-        let mainFUSDVault = signer.borrow<&FUSD.Vault>(from: FUSDVaultStoragePath)
+        let mainFlowVault = acct.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
             ?? panic("Cannot borrow FlowToken vault from acct storage")
-        self.paymentVault <- mainFUSDVault.withdraw(amount: price)
+        self.paymentVault <- mainFlowVault.withdraw(amount: price)
 
-        self.AssetsCollection = signer.borrow<&Gaia.Collection{NonFungibleToken.Receiver}>(
+        self.GaiaCollection = acct.borrow<&Gaia.Collection{NonFungibleToken.Receiver}>(
             from: Gaia.CollectionStoragePath
-        ) ?? panic("Cannot borrow Assets collection receiver from acct")
+        ) ?? panic("Cannot borrow NFT collection receiver from account")
     }
 
     execute {
-        self.marketCollection.purchase(
-            itemID: saleAssetID,
-            buyerCollection: self.AssetsCollection,
-            buyerPayment: <- self.paymentVault
+        let item <- self.listing.purchase(
+            payment: <-self.paymentVault
         )
+        
+        self.GaiaCollection.deposit(token: <-item)
+        self.storefront.cleanup(listingResourceID: listingResourceID)
     }
 }
 `;
 
-export async function buy(saleAssetID, address) {
+export async function buy(listingResourceID, storefrontAddress) {
+  if (listingResourceID == null)
+    throw new Error('buy(listingResourceID, storefrontAddress) -- listingResourceID required');
+  if (storefrontAddress == null)
+    throw new Error('buy(listingResourceID, storefrontAddress) -- storefrontAddress required');
   try {
     const txId = await fcl
       .send([
@@ -50,7 +57,7 @@ export async function buy(saleAssetID, address) {
         fcl.payer(fcl.authz),
         fcl.proposer(fcl.authz),
         fcl.authorizations([fcl.authz]),
-        fcl.args([fcl.arg(saleAssetID, t.UInt64), fcl.arg(address, t.Address)]),
+        fcl.args([fcl.arg(listingResourceID, t.UInt64), fcl.arg(storefrontAddress, t.Address)]),
         fcl.limit(1000)
       ])
       .then(fcl.decode);
--- src/flow/setupAccount.js
+++ src/flow/setupAccount.js
@@ -1,104 +1,59 @@
 import { fcl } from '../config/config';
 
 const TX = fcl.cdc`
-import Profile from 0xProfile
-import FungibleToken from 0xFungibleToken
-import NonFungibleToken from 0xNFTInterface
-import FlowToken from 0xFlowToken
-import Gaia from 0xNFTContract
-import GaiaMarket from 0xNFTMarket
-import FUSD from 0xFUSDContract
+import NFTStorefront from "../contracts/NFTStorefront.cdc"
+import Profile from "../contracts/Profile.cdc"
+import FlowToken from "../contracts/FlowToken.cdc"
+import FungibleToken from "../contracts/FungibleToken.cdc"
 
-// This transaction configures an account to hold assets.
+// This transaction installs the Storefront ressource in an account.
+
 transaction {
-
     let address: Address
 
-    prepare(account: AuthAccount) {
-      let FlowTokenReceiverPublicPath = /public/flowTokenReceiver
-      let FlowTokenVaultStoragePath = /storage/flowTokenVault
-      //INITIALIZING PARAMS
-      self.address = account.address
-        
-        // Init Profile
-        if (!Profile.check(self.address)) {
-          // This creates and stores the Profile in the users account
-          account.save(<- Profile.new(), to: Profile.privatePath)
+    prepare(acct: AuthAccount) {
+        self.address = acct.address
 
-          // This creates the public capability that lets applications read the profiles info
-          account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
-        }
+          // Init Profile
+          if (!Profile.check(self.address)) {
+            // This creates and stores the Profile in the users account
+            acct.save(<- Profile.new(), to: Profile.privatePath)
 
-        // First, check to see if a moment collection already exists
-        if account.borrow<&Gaia.Collection>(from: Gaia.CollectionStoragePath) == nil {
-            // create a new Gaia Collection
-            let collection <- Gaia.createEmptyCollection() as! @Gaia.Collection
-            // Put the new Collection in storage
-            account.save(<-collection, to: Gaia.CollectionStoragePath)
-            // create a public capability for the collection
-            account.link<&{Gaia.CollectionPublic}>(Gaia.CollectionPublicPath, target: Gaia.CollectionStoragePath)
-        }
+            // This creates the public capability that lets applications read the profiles info
+            acct.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+          }
+          
+          // If the account doesn't already have a Storefront
+          if acct.borrow<&NFTStorefront.Storefront>(from: NFTStorefront.StorefrontStoragePath) == nil {
+              // Create a new empty .Storefront
+              let storefront <- NFTStorefront.createStorefront() as! @NFTStorefront.Storefront
+              
+              // save it to the account
+              acct.save(<-storefront, to: NFTStorefront.StorefrontStoragePath)
 
-        // Init Gaia Market collection
-        if account.borrow<&GaiaMarket.Collection>(from: GaiaMarket.CollectionStoragePath) == nil {
-
-            // create a new empty collection
-            let collection <- GaiaMarket.createEmptyCollection() as! @GaiaMarket.Collection
-            
-            // save it to the account
-            account.save(<-collection, to: GaiaMarket.CollectionStoragePath)
-
-            // create a public capability for market the collection
-            account.link<&GaiaMarket.Collection{GaiaMarket.CollectionPublic}>(GaiaMarket.CollectionPublicPath, target: GaiaMarket.CollectionStoragePath)
-        
-            // create a public capability for the collection
-            if !account.getCapability<&Gaia.Collection{NonFungibleToken.Provider}>(/private/GaiaCollectionProvider)!.check() {
-              account.link<&Gaia.Collection{NonFungibleToken.Provider}>(/private/GaiaCollectionProvider, target: Gaia.CollectionStoragePath)
-            }
-  
+              // create a public capability for the .Storefront
+              acct.link<&NFTStorefront.Storefront{NFTStorefront.StorefrontPublic}>(NFTStorefront.StorefrontPublicPath, target: NFTStorefront.StorefrontStoragePath)
           }
 
-        //Init FUSD Balance
-        if account.borrow<&FUSD.Vault>(from: /storage/fusdVault) == nil {
-          
-          // Create a new FUSD Vault and put it in storage
-          account.save(<-FUSD.createEmptyVault(), to: /storage/fusdVault)
-      
-          // Create a public capability to the Vault that only exposes
-          // the deposit function through the Receiver interface
-          account.link<&FUSD.Vault{FungibleToken.Receiver}>(
-            /public/fusdReceiver,
-            target: /storage/fusdVault
-          )
-      
-          // Create a public capability to the Vault that only exposes
-          // the balance field through the Balance interface
-          account.link<&FUSD.Vault{FungibleToken.Balance}>(
-            /public/fusdBalance,
-            target: /storage/fusdVault
-          )
-         
-      }
-      //Init Flow Token Balance
-      if account.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault) == nil {
-        // Create a new flowToken Vault and put it in storage
-        account.save(<-FlowToken.createEmptyVault(), to: /storage/flowTokenVault)
+          if acct.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault) == nil {
+            // Create a new flowToken Vault and put it in storage
+            acct.save(<-FlowToken.createEmptyVault(), to: /storage/flowTokenVault)
 
-        // Create a public capability to the Vault that only exposes
-        // the deposit function through the Receiver interface
-        account.link<&FlowToken.Vault{FungibleToken.Receiver}>(
-            /public/flowTokenReceiver,
-            target: /storage/flowTokenVault
-        )
+            // Create a public capability to the Vault that only exposes
+            // the deposit function through the Receiver interface
+            acct.link<&{FungibleToken.Receiver}>(
+                /public/flowTokenReceiver,
+                target: /storage/flowTokenVault
+            )
 
-        // Create a public capability to the Vault that only exposes
-        // the balance field through the Balance interface
-        account.link<&FlowToken.Vault{FungibleToken.Balance}>(
-            /public/flowTokenBalance,
-            target: /storage/flowTokenVault
-        )
+            // Create a public capability to the Vault that only exposes
+            // the balance field through the Balance interface
+            acct.link<&{FungibleToken.Balance}>(
+                /public/flowTokenBalance,
+                target: /storage/flowTokenVault
+            )
+        }
     }
-  }
 }
 `;
 
