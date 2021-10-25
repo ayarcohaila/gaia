f364e00207d40bd597472ee6322db23c5e034a07
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-10-25 14:05:27 -0300

    feat: Add empty message for profile nft list

--- src/components/profileList/index.jsx
+++ src/components/profileList/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { ProfileCard } from '~/components';
+import { Typography } from '@mui/material';
+import NextLink from 'next/link';
 
 const ProfileList = ({ nfts }) => {
   return (
     <>
-      {nfts.map(nft => (
-        <ProfileCard key={nft.asset_id} data={nft} />
-      ))}
+      {nfts.length > 0 ? (
+        nfts.map(nft => <ProfileCard key={nft.asset_id} data={nft} />)
+      ) : (
+        <Typography mt="42px" variant="body">
+          Unfortunately, you don&apos;t have any Nft. You can check the current offers on{' '}
+          <NextLink href="/ballerz">marketplace</NextLink>.
+        </Typography>
+      )}
     </>
   );
 };
