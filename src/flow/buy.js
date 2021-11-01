67f60a00f0f60ef111f93b01b01192f88d332e55
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-01 14:02:17 -0300

    Update buy transaction

--- src/flow/buy.js
+++ src/flow/buy.js
@@ -1,10 +1,22 @@
 import { fcl, t } from '../config/config';
 
-export async function buy(tx, listingResourceID, ownerAddress) {
+export async function buy(tx, listingResourceID, ownerAddress, expectedPrice, buyerAddress) {
   if (listingResourceID == null)
-    throw new Error('buy(listingResourceID, ownerAddress) -- listingResourceID required');
+    throw new Error(
+      'buy(listingResourceID, ownerAddress, expectedPrice, buyerAddress) -- listingResourceID required'
+    );
   if (ownerAddress == null)
-    throw new Error('buy(listingResourceID, ownerAddress) -- ownerAddress required');
+    throw new Error(
+      'buy(listingResourceID, ownerAddress, expectedPrice, buyerAddress) -- ownerAddress required'
+    );
+  if (expectedPrice == null)
+    throw new Error(
+      'buy(listingResourceID, ownerAddress, expectedPrice, buyerAddress) -- expectedPrice required'
+    );
+  if (buyerAddress == null)
+    throw new Error(
+      'buy(listingResourceID, ownerAddress, expectedPrice, buyerAddress) -- buyerAddress required'
+    );
   try {
     const txId = await fcl
       .send([
@@ -12,7 +24,12 @@ export async function buy(tx, listingResourceID, ownerAddress) {
         fcl.payer(fcl.authz),
         fcl.proposer(fcl.authz),
         fcl.authorizations([fcl.authz]),
-        fcl.args([fcl.arg(listingResourceID, t.UInt64), fcl.arg(ownerAddress, t.Address)]),
+        fcl.args([
+          fcl.arg(listingResourceID, t.UInt64),
+          fcl.arg(ownerAddress, t.Address),
+          fcl.arg(expectedPrice, t.UFix64),
+          fcl.arg(buyerAddress, t.Address)
+        ]),
         fcl.limit(1000)
       ])
       .then(fcl.decode);
