b0452ef3af043c5198b9fdc9ee473594fedaa51f
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-03 15:58:55 -0300

    chore: Remove wallet address from banner

--- src/components/collectionBannerAccount/index.jsx
+++ src/components/collectionBannerAccount/index.jsx
@@ -1,30 +1,10 @@
-import { useState } from 'react';
-import { Box, Tooltip } from '@mui/material';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-
+import { Box } from '@mui/material';
 import * as Styled from './styles';
 
-function BannerAccount(props) {
-  const defaultTooltipTitle = 'Copy to clipboard';
-  const [tooltipTitle, setTooltipTitle] = useState(defaultTooltipTitle);
-  const { accountNumber, bannerName } = props;
-
-  const secretAccount = accountNumber?.substring(0, 5) + '...' + accountNumber?.substring(6, 10);
-
-  const copyHandler = () => {
-    navigator.clipboard.writeText(accountNumber);
-    setTooltipTitle('Copied!');
-  };
-
+function BannerAccount({ bannerName }) {
   return (
-    <Box>
+    <Box my="auto">
       <Styled.BannerName>{'@' + bannerName?.toUpperCase()}</Styled.BannerName>
-      <Tooltip title={tooltipTitle} onOpen={() => setTooltipTitle(defaultTooltipTitle)}>
-        <Styled.BannerAccountStyled onClick={copyHandler} bgColor={props.bgColor}>
-          <span>{secretAccount}</span>
-          <ContentCopyIcon style={{ width: '12px', color: '#fff', transform: 'rotateY(170deg)' }} />
-        </Styled.BannerAccountStyled>
-      </Tooltip>
     </Box>
   );
 }
