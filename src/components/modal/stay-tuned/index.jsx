4eabfa8dfb6b25963f72e2699fc312f31467fc51
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-03 13:04:01 -0400

    refactor: style text in the descriptionSx

--- src/components/modal/stay-tuned/index.jsx
+++ src/components/modal/stay-tuned/index.jsx
@@ -5,23 +5,22 @@ import Modal from '..';
 
 const StayTunedModal = ({ onClose, ...props }) => {
   const title = 'Sell NFT';
-  const description = () => (
-    <>
+  const description = `
       Want to sell an NFT you purchased on Gaia?
-      <br /> Stay tuned -- our secondary marketplace is coming <br /> in December!
-      <br />
-      <br /> In the meantime, you can transfer NFTs by going to <br /> My Account, clicking
-      “Transfer” on the desired NFT,
-      <br /> and entering the wallet address of the recipient.
-    </>
-  );
+    Stay tuned -- our secondary marketplace is coming
+    in December!
+    
+    In the meantime, you can transfer NFTs by going to
+    My Account, clicking “Transfer” on the desired NFT,
+    and entering the wallet address of the recipient.
+  `;
 
   return (
     <Modal
-      description={description()}
+      description={description}
       onClose={onClose}
       title={title}
-      descriptionSx={{ maxWidth: '440px', textAlign: 'center', mb: 0 }}
+      descriptionSx={{ maxWidth: '440px', whiteSpace: 'pre', textAlign: 'center', mb: 0 }}
       height="318px"
       mobileHeight="50vh"
       titleSx={{ mt: 0, mb: '20px' }}
