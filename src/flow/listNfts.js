626c0182687d1c7b2fbb4efa3ac09b103cad9bed
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-09 17:43:29 -0300

    feat: Update ipfs gateway

--- src/flow/listNfts.js
+++ src/flow/listNfts.js
@@ -3,7 +3,7 @@ import { fcl, t } from '../config/config';
 const LIST_NFTS_SCRIPT = fcl.cdc`
         import NonFungibleToken from 0xNFTInterface
         import Gaia from 0xGaiaContract
-        
+
         // Print the NFTs owned by account.
         // pub fun main(address: Address): [UInt64] {
 
@@ -11,7 +11,7 @@ const LIST_NFTS_SCRIPT = fcl.cdc`
           pub let id: String?
           pub let name: String?
           pub let imageURL: String
-          
+
           init(id: String?, name: String?, imageURL: String) {
               self.id = id
               self.name = name
@@ -25,33 +25,33 @@ const LIST_NFTS_SCRIPT = fcl.cdc`
             let accountSetup = account
               .getCapability<&{Gaia.CollectionPublic}>(Gaia.CollectionPublicPath)
               .check()
-        
+
             if accountSetup {
               let collectionRef = account.getCapability(Gaia.CollectionPublicPath)
                                       .borrow<&{Gaia.CollectionPublic}>()!
-          
+
               let ids = collectionRef.getIDs()
-          
+
               for assetID in ids {
-  
+
                 let asset = collectionRef.borrowGaiaNFT(id: assetID)!
-  
+
                 if asset.data.setID == setID {
                   let templateMetadata = Gaia.getTemplateMetaData(templateID: asset.data.templateID)
                   let id = Gaia.getTemplateMetaDataByField(templateID: asset.data.templateID, field: "id")
                   let title = Gaia.getTemplateMetaDataByField(templateID: asset.data.templateID, field: "title")
                   let imageURL = Gaia.getTemplateMetaDataByField(templateID: asset.data.templateID, field: "img")!
-                  
+
                   let nftData = NFTData(
                     id: id,
                     name: title,
-                    imageURL: "https://ipfs.fleek.co/ipfs/".concat(imageURL.slice(from: 7, upTo: imageURL.length))
+                    imageURL: "https://images.ongaia.com/ipfs/".concat(imageURL.slice(from: 7, upTo: imageURL.length))
                   )
-                    
+
                   assets.append(nftData)
-                } 
+                }
               }
-            } 
+            }
             return assets
         }
 `;
