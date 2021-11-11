208715d702a8b517bf8531712b1c6351b2f75d62
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-11 14:33:05 -0300

    chore: Unwrap wallet addresses

--- src/components/product-details/top-section/blockchain-history/index.jsx
+++ src/components/product-details/top-section/blockchain-history/index.jsx
@@ -3,43 +3,34 @@ import { Box, Grid, Typography, useTheme } from '@mui/material';
 import PropTypes from 'prop-types';
 
 import { useBreakpoints } from '~/hooks';
-import {
-  convertCamelCaseToSentenceCase,
-  formatDate,
-  parseStringWithEllipsis
-} from '~/utils/string';
+import { convertCamelCaseToSentenceCase, formatDate } from '~/utils/string';
 
 const BlockchainHistory = ({ data }) => {
   const {
     palette: { grey }
   } = useTheme();
-
   const { isSmallDevice } = useBreakpoints();
 
   const parsedData = {
-    creator: parseStringWithEllipsis(data?.creator, 5, 3),
-    owner: parseStringWithEllipsis(data?.owner, 5, 3),
+    ...data,
     mintDate: formatDate(data?.mintDate),
-    lastActivity: formatDate(data?.lastActivity),
-    contract: parseStringWithEllipsis(data?.contract, 5, 3)
+    lastActivity: formatDate(data?.lastActivity)
   };
 
   return (
-    <Box
-      bgcolor={grey[200]}
-      borderRadius="14px"
-      p={isSmallDevice ? '24px 22px' : '20px 100px 20px 24px'}>
+    <Box bgcolor={grey[200]} borderRadius="14px" p={isSmallDevice ? '24px 22px' : '20px 32px'}>
       {Object.entries(parsedData).map(([key, value], index) => (
         <Grid
           alignItems="center"
           container
-          key={key}
           justifyContent="space-between"
-          mt={index ? 3.5 : 0}>
+          key={key}
+          mt={index ? 2.8 : 0}
+          width="90%">
           <Typography color={grey[500]} variant="subtitle1" fontWeight="normal">
             {convertCamelCaseToSentenceCase(key)}:
           </Typography>
-          <Box width="150px">
+          <Box width={isSmallDevice ? '55%' : '45%'} sx={{ wordBreak: 'break-word' }}>
             <Typography color={grey[600]} variant="subtitle1" textAlign="left">
               {value}
             </Typography>
