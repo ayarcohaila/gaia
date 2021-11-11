4b3c400b156383ba2ea9ae722ce1391d807b87e7
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-11 14:50:57 -0300

    chore: Set max height

--- src/components/accordion/styles.js
+++ src/components/accordion/styles.js
@@ -3,6 +3,7 @@ import { Accordion as MuiAccordion, styled } from '@mui/material';
 export const Accordion = styled(MuiAccordion)(() => ({
   border: 0,
   boxShadow: 'none',
+  maxHeight: '530px',
 
   '&::before': {
     display: 'none'
