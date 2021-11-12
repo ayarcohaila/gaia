051a86a3a109e892fae663e4664365b77c2e0b84
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-12 15:02:14 -0300

    feat: adding security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,39 @@
 const withPlugins = require('next-compose-plugins');
 const { withSentryConfig } = require('@sentry/nextjs');
 
+// Reference: https://nextjs.org/docs/advanced-features/security-headers
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
+];
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      }
+    ];
+  },
   async redirects() {
     return [
       {
