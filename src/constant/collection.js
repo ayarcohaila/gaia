7c4bea92ba6c7db1b090a6000e6fb0f99416bb94
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-11-19 12:26:07 -0300

    Update collection information

--- src/constant/collection.js
+++ src/constant/collection.js
@@ -10,7 +10,7 @@ export const COLLECTIONS = {
 
 export const COLLECTION_ID = {
   [COLLECTIONS.BALLERZ]: ballerzCollection || 'db4ccc58-4398-4a66-87cd-5b0f6c6c21f3',
-  [COLLECTIONS.BRYSON]: '3172a9dd-2143-4087-8ecb-da940a65ca5d'
+  [COLLECTIONS.BRYSON]: 'db6b606b-f08b-439d-80f0-8ebc0ca9f837'
 };
 
 export const COLLECTION_TOTAL_NUMBER = {
