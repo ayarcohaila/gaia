12b7f2f584df1ec1d1f0bc8d362a9ab8d44c8eef
Ayar Cohaila <ayarcohaila0912@gmail.com>
2021-12-16 10:48:47 -0300

    Change the mobile menu, if logged out, to use the new title

--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -115,7 +115,8 @@ const Header = () => {
                 variant="text"
                 onClick={handleClick}
                 onMouseEnter={() => handleDropdownMenu(true)}
-                data-id={USER_MENU_IDS.PROFILE}>
+                data-id={USER_MENU_IDS.PROFILE}
+              >
                 <Styled.UserAvatar alt="User Icon" />
                 <Styled.AvatarMoreIcon rotate={!!openUserMenu} />
               </Styled.AvatarButton>
@@ -142,7 +143,8 @@ const Header = () => {
           sx={{ gap: '24px' }}
           alignItems="center"
           justifyContent="center"
-          direction="column">
+          direction="column"
+        >
           <Button variant="text" disableRipple onClick={navigateToHome}>
             <NextImage width={102.4} height={40} src="/static/img/gaia_logo-black.png" />
           </Button>
@@ -152,20 +154,23 @@ const Header = () => {
                 <Styled.ButtonText
                   variant="text"
                   onClick={handleClick}
-                  data-id={USER_MENU_IDS.BROWSE}>
+                  data-id={USER_MENU_IDS.BROWSE}
+                >
                   {MARKETPLACE_TITLE}
                 </Styled.ButtonText>
               )}
               <Styled.ButtonText
                 variant="text"
                 onClick={handleClick}
-                data-id={USER_MENU_IDS.PROFILE}>
+                data-id={USER_MENU_IDS.PROFILE}
+              >
                 Profile
               </Styled.ButtonText>
               <Styled.ButtonText
                 variant="text"
                 onClick={handleClick}
-                data-id={USER_MENU_IDS.DAPPER_WALLET}>
+                data-id={USER_MENU_IDS.DAPPER_WALLET}
+              >
                 My Wallet
               </Styled.ButtonText>
               <Styled.ButtonText variant="text" onClick={handleClick}>
@@ -176,7 +181,7 @@ const Header = () => {
             <>
               {hasSecondarySale && (
                 <Styled.ButtonText variant="text" onClick={handleClick}>
-                  Browse All NFTs
+                  {MARKETPLACE_TITLE}
                 </Styled.ButtonText>
               )}
               <Styled.CustomButton variant="contained" headerModal onClick={login}>
