4e80595f73d1d93e9dd0a06ae28ffd983dc6efac
Ayar Cohaila <ayarcohaila0912@gmail.com>
2022-01-19 16:56:44 -0400

    Fix size on details page

--- src/components/productDetails/topSection/asset/styles.js
+++ src/components/productDetails/topSection/asset/styles.js
@@ -1,11 +1,21 @@
 import { styled, Box } from '@mui/material';
 
-export const ImageContainer = styled(Box)(() => ({
+export const ImageContainer = styled(Box)(({ theme }) => ({
   borderRadius: '16px',
-  height: '424px',
   position: 'relative',
   margin: '0 auto',
+  height: '424px',
   width: '424px',
+
+  [theme.breakpoints.down('lg')]: {
+    height: '380px',
+    width: '380px'
+  },
+
+  [theme.breakpoints.down('sm')]: {
+    height: '275px',
+    width: '275px'
+  },
 
   img: {
     borderRadius: '16px'
