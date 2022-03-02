32d3bd052036b66b6bbe79102995b0d5418c6633
Ayar Cohaila <ayarcohaila0912@gmail.com>
2022-03-02 12:08:38 -0300

    feat(GAIA-72): added a custom 404 page and its styles (#661)
    * feat: added a custom 404 page and its styles
    
    * styles: added text-align center to the text component
    
    * fix: changed js to ts
    
    * fix: safari compatibility
    
    * fix: safari compatibility

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import { Text, Wrapper } from '../styles/404/styles';
+
+const NotFoundPage = () => (
+  <Wrapper>
+    <Text>404 - This page could not be found.</Text>
+  </Wrapper>
+);
+
+export default NotFoundPage;
