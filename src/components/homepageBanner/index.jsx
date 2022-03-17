e45ab397077e0fa4795b8e32e961440a7ee73512
Ayar Cohaila <ayarcohaila0912@gmail.com>
2022-03-17 16:17:38 -0300

    add right click on banner (#698)
    Co-authored-by: Edison Cristovão <edisoncristovao@MacBook-Pro-de-Edison.local>

--- src/components/homepageBanner/index.jsx
+++ src/components/homepageBanner/index.jsx
@@ -24,42 +24,46 @@ export default function HomepageBanner() {
   };
 
   return (
-    <Styled.BannerRedirect
-      disableTouchRipple
-      onClick={navigateToSneakerz}
-      arial-label="Redirect to Sneakerz page">
-      <Styled.ContainerBackground alt="background" imgUrl={bannerData.background}>
-        <Styled.Container>
-          <Styled.TypographyWithOpacity
-            variant={isMediumDevice ? 'h5' : 'h4'}
-            fontWeight="normal"
-            lineHeight="24px"
-            mt={isExtraSmallDevice ? '80px' : isMediumDevice && '120px'}>
-            {bannerData.drop}
-          </Styled.TypographyWithOpacity>
-          <Typography variant="h2" fontWeight="bold" maxWidth="450px" mt="10px">
-            {bannerData.title}
-          </Typography>
-          <Typography
-            variant={isMediumDevice ? 'subtitle1' : 'h4'}
-            fontWeight="normal"
-            mt="10px"
-            lineHeight={isMediumDevice && '20px'}
-            mb={isMediumDevice && '24px'}
-            sx={{ maxWidth: '600px', lineHeight: !isMediumDevice && '30px' }}>
-            {bannerData.description}
-          </Typography>
-          {!hasBrowse && (
-            <Grid container sx={{ gap: '12px' }}>
-              <Link href="/sneakerz" passHref>
-                <Styled.CardButton component="a" aria-label="sneakerz" sx={{ width: '200px' }}>
-                  <Typography variant="subtitle1">Explore Sneakerz</Typography>
-                </Styled.CardButton>
-              </Link>
-            </Grid>
-          )}
-        </Styled.Container>
-      </Styled.ContainerBackground>
-    </Styled.BannerRedirect>
+    <Link href={'/sneakerz'} passHref>
+      <Styled.BannerRedirect
+        component="a"
+        href="/sneakerz"
+        sx={{
+          textDecoration: 'none'
+        }}>
+        <Styled.ContainerBackground alt="background" imgUrl={bannerData.background}>
+          <Styled.Container>
+            <Styled.TypographyWithOpacity
+              variant={isMediumDevice ? 'h5' : 'h4'}
+              fontWeight="normal"
+              lineHeight="24px"
+              mt={isExtraSmallDevice ? '80px' : isMediumDevice && '120px'}>
+              {bannerData.drop}
+            </Styled.TypographyWithOpacity>
+            <Typography variant="h2" fontWeight="bold" maxWidth="450px" mt="10px">
+              {bannerData.title}
+            </Typography>
+            <Typography
+              variant={isMediumDevice ? 'subtitle1' : 'h4'}
+              fontWeight="normal"
+              mt="10px"
+              lineHeight={isMediumDevice && '20px'}
+              mb={isMediumDevice && '24px'}
+              sx={{ maxWidth: '600px', lineHeight: !isMediumDevice && '30px' }}>
+              {bannerData.description}
+            </Typography>
+            {!hasBrowse && (
+              <Grid container sx={{ gap: '12px' }}>
+                <Link href="/sneakerz" passHref>
+                  <Styled.CardButton component="a" aria-label="sneakerz" sx={{ width: '200px' }}>
+                    <Typography variant="subtitle1">Explore Sneakerz</Typography>
+                  </Styled.CardButton>
+                </Link>
+              </Grid>
+            )}
+          </Styled.Container>
+        </Styled.ContainerBackground>
+      </Styled.BannerRedirect>
+    </Link>
   );
 }
